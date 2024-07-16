3f2d69bfbc73a8929b00693597da78339c96fc63
Pablo <pablo.henrique.prestes@gmail.com>
2024-07-15 23:30:41 -0300

    Commit inicial

---
+++ EstudosMaui/EstudosMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace EstudosMaui
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new AppShell();
+		}
+	}
+}
---
+++ EstudosMaui/EstudosMaui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace EstudosMaui
+{
+	public partial class AppShell : Shell
+	{
+		public AppShell()
+		{
+			InitializeComponent();
+		}
+	}
+}
